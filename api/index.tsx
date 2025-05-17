1846e5762a7d2d4f384888ef2bc2cf94a279f7ee
Lo <l709937065@gmail.com>
2025-05-18 01:12:03 +0800

    chore: init

---
+++ api/index.tsx
@@ -0,0 +1,56 @@
+import { Hono } from "hono"
+import { handle } from "hono/vercel"
+import { execa } from "execa"
+
+const app = new Hono().basePath("/api")
+
+app.get("/", (c) => {
+  return c.html(
+    <html>
+      <body>
+        <form action="/api/parse" method="post">
+          <label for="url">URL:</label>
+          <input type="text" name="url" />
+          <button type="submit">Send</button>
+        </form>
+      </body>
+    </html>
+  )
+})
+
+app.post("/parse", async (c) => {
+  const formData = await c.req.formData()
+  const url = formData.get("url")
+
+  if (!url) {
+    return c.json({
+      msg: "URL is required",
+    })
+  }
+
+  if (typeof url !== "string") {
+    return c.json({
+      msg: "URL must be a string",
+    })
+  }
+
+  const { stdout, stderr } = await execa("npx", [
+    "single-file-cli",
+    url,
+    "--dump-content",
+  ])
+
+  if (stderr) {
+    return c.json({ msg: stderr })
+  }
+
+  return c.json({ msg: "ok", parsed: stdout })
+})
+
+const handler = handle(app)
+
+export const GET = handler
+export const POST = handler
+export const PATCH = handler
+export const PUT = handler
+export const OPTIONS = handler
